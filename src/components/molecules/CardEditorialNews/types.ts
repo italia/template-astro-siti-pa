6dbc2723b958b6888532eb940010acb22aebb803
mariateresagraziano <marytex.dev@gmail.com>
2025-11-26 13:49:10 +0100

    fix:button variant

---
+++ src/components/molecules/CardEditorialNews/types.ts
@@ -0,0 +1,11 @@
+export type CardEditorialNewsProps = {
+  title: string;
+  description: string;
+  imageUrl: string;
+  imageAlt?: string;
+  linkTo: string;
+  category: string;
+  dateTime: string;
+  formatDate: string;
+  action: string;
+};
