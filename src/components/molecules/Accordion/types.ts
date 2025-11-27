3dadb3f76ae31df3ce2f23321f6994d880062180
mariateresagraziano <marytex.dev@gmail.com>
2025-11-27 09:42:15 +0100

    feat: add text and accordions section

--- src/components/molecules/Accordion/types.ts
+++ src/components/molecules/Accordion/types.ts
@@ -1,3 +1,9 @@
+export type AccordionProps = {
+  id: string;
+  items: AccordionItemProps[];
+  variant?: "default" | "background-active";
+};
+
 export type AccordionItemProps = {
   header: string;
   body: string;
---
+++ src/components/organisms/TextAndAccordions/index.ts
@@ -0,0 +1,1 @@
+export { default as TextAndAccordions } from "./TextAndAccordions.astro";
