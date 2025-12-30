b41c4c2e57849ef28779246c872390e94a36b78b
Daniele Luconi <daniele.luconi@claranet.com>
2025-12-30 17:51:49 +0100

    paginated news react

---
+++ src/components/react/Chip/Chip.tsx
@@ -0,0 +1,51 @@
+import "./style.scss";
+
+type ChipProps = {
+  label: string;
+  visuallyHidden: string;
+  disabled?: boolean;
+  active?: boolean;
+  variant?: "primary" | "standard" | "outline-white";
+  size?: "default" | "large";
+};
+
+const sizeMap = {
+  default: "",
+  large: "chip-lg",
+};
+
+const variantMap = {
+  standard: "",
+  "outline-white": "chip-outline-white",
+  primary: "chip-primary",
+};
+
+export function Chip({
+  label,
+  visuallyHidden,
+  variant = "standard",
+  disabled = false,
+  size = "default",
+  active = false,
+}: ChipProps) {
+  const sizeClass = sizeMap[size];
+  const chipVariant = variantMap[variant];
+  const disabledClass = disabled ? "chip-disabled" : "";
+  const className = [
+    "chip",
+    "chip-simple",
+    chipVariant,
+    disabledClass,
+    sizeClass,
+    active ? "active" : "",
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <button disabled={disabled} className={className}>
+      <span className="visually-hidden">{visuallyHidden}</span>
+      <span className="chip-label">{label}</span>
+    </button>
+  );
+}
--- src/components/react/PaginatedNews/PaginatedNews.tsx
+++ src/components/react/PaginatedNews/PaginatedNews.tsx
@@ -4,13 +4,25 @@ import {
   CardEditorialNews,
   type CardEditorialNewsProps,
 } from "@components/react/CardEditorialNews";
+import { Chip } from "@components/react/Chip";
 
 type PaginatedNewsProps = {
+  title: string;
+  paragraph: string;
+  filterTitle: string;
+  labelForAll: string;
   news: CardEditorialNewsProps[];
   perPage?: number;
 };
 
-export function PaginatedNews({ news, perPage = 6 }: PaginatedNewsProps) {
+export function PaginatedNews({
+  news,
+  perPage = 6,
+  title,
+  paragraph,
+  filterTitle,
+  labelForAll,
+}: PaginatedNewsProps) {
   const [page, setPage] = useState(1);
 
   const totalPages = Math.ceil(news.length / perPage);
@@ -18,10 +30,26 @@ export function PaginatedNews({ news, perPage = 6 }: PaginatedNewsProps) {
   const paginatedNews = news.slice(start, start + perPage);
 
   return (
-    <div>
+    <div className="container py-80">
+      <div className="row justify-content-between align-items-center">
+        <div className="col-lg-5 col-12">
+          <div className={`text-container mb-4 mb-lg-0`}>
+            <h2 className="mb-3">{title}</h2>
+            <div className="mb-4">{paragraph}</div>
+          </div>
+        </div>
+        <div className="col-lg-5 col-12">
+          <p className="fw-semibold fs-6 text-uppercase">{filterTitle}</p>
+          <Chip
+            label={labelForAll}
+            variant="primary"
+            visuallyHidden={labelForAll}
+          />
+        </div>
+      </div>
       <ul className="it-card-list row">
         {paginatedNews.map((n, idx) => (
-          <li className="col-12 col-lg-4 mb-3" key={idx}>
+          <li className="col-12 col-lg-4 mb-3" key={n.title}>
             <CardEditorialNews {...n} />
           </li>
         ))}
