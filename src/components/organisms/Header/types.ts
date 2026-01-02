4483f21039a97db25dc6c832461e5865766fa6a7
mariateresagraziano <marytex.dev@gmail.com>
2026-01-02 13:39:01 +0100

    feat: add secondary navigation in header

--- src/components/organisms/Header/types.ts
+++ src/components/organisms/Header/types.ts
@@ -3,19 +3,25 @@ export type HeaderProps = {
   brandTitle: string;
   brandTagLine: string;
   locales: string[];
-  menuItems: MenuItemsProps[];
+  menuItems: HeaderNavbarProps;
+  linkBackHome: string;
 };
 
-export type MenuItemsProps = {
+export type MenuItemProps = {
   id: string;
   url: string;
   active: boolean;
   title: string;
 };
 
+export type HeaderNavbarProps = {
+  left: MenuItemProps[];
+  right?: MenuItemProps[];
+};
+
 export type HeaderCentralProps = {
   brandTitle: string;
   brandTagLine: string;
-  menuItems: MenuItemsProps[];
+  menuItems: HeaderNavbarProps;
   linkBackHome: string;
 };
