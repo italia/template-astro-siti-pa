eac0751a451f23c0709b0a80ea2dd3ff5df030ad
mariateresagraziano <marytex.dev@gmail.com>
2026-01-04 15:34:04 +0100

    feat: add list collection

---
+++ src/components/molecules/ListCollection/index.ts
@@ -0,0 +1,1 @@
+export { default as ListCollection } from "./ListCollection.astro";
---
+++ src/components/molecules/ListCollection/types.ts
@@ -0,0 +1,7 @@
+import type { CardEditorialNewsProps } from "../CardEditorialNews/types";
+
+export type ListCollectionProps = {
+  title: string;
+  paragraph: string;
+  items: CardEditorialNewsProps[];
+};
