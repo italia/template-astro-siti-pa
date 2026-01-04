f68f7732bab87646a67e5aeedc77b8b19e8950b2
mariateresagraziano <marytex.dev@gmail.com>
2026-01-04 15:35:28 +0100

    feat: update hero

--- src/components/organisms/Hero/types.ts
+++ src/components/organisms/Hero/types.ts
@@ -1,4 +1,5 @@
 import type { ImageProps } from "@components/atoms/Image/types";
+import type { SectionBackground } from "@components/layout/SplitContent/SplitContent.astro";
 
 export type HeroProps = {
   title: string;
@@ -7,6 +8,13 @@ export type HeroProps = {
   size?: HeroSizeProps;
   category?: string;
   showBreadcrumb?: boolean;
+  variant?: HeroVariantProps;
+  backgroundColor?: SectionBackground;
 };
 
 export type HeroSizeProps = "xsmall" | "small" | "default";
+export type HeroVariantProps =
+  | "default"
+  | "small"
+  | "xsmall-full"
+  | "xsmall-compact";
