645c3cea2353e2b56ce64c39115af01465fe04e6
mariateresagraziano <marytex.dev@gmail.com>
2026-01-12 08:15:44 +0100

    fix: update name props

--- src/components/molecules/ImageContainer/types.ts
+++ src/components/molecules/ImageContainer/types.ts
@@ -2,7 +2,7 @@ import type { ImageProps } from "@components/atoms/Image/types";
 
 export type RatioImageProps = "4x3" | "1x1" | "16x9";
 
-export type ImageWithRatioProps = {
+export type ImageContainerProps = {
   ratio?: RatioImageProps;
   image: ImageProps;
 };
