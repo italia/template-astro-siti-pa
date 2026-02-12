b92c606e800e3d9b26fce73ff67166a28170f436
mariateresagraziano <marytex.dev@gmail.com>
2026-02-12 12:36:45 +0100

    fix: import netlify in production branch

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,16 @@
+import netlify from "@astrojs/netlify";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { resolve } from "path";
-import sitemap from "@astrojs/sitemap";
-
-import vercel from "@astrojs/vercel";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL,
   integrations: [sitemap(), react()],
-  adapter: vercel(),
+  adapter: netlify({
+    imageCDN: false,
+  }),
   vite: {
     resolve: {
       alias: {
--- src/pages/api/draft-mode/enable/index.ts
+++ src/pages/api/draft-mode/enable/index.ts
@@ -1,26 +1,21 @@
-import type { APIRoute } from "astro";
 import { enableDraftMode } from "@lib/draftMode";
 import {
   handleUnexpectedError,
   invalidRequestResponse,
   isRelativeUrl,
 } from "@utils/apiUtils";
+import type { APIRoute } from "astro";
 
 export const prerender = false;
 
 export const GET: APIRoute = (event) => {
   const { url } = event;
 
-  const token = url.searchParams.get("token");
   const redirectUrl = url.searchParams.get("url") || "/";
   const type = url.searchParams.get("type");
   const id = url.searchParams.get("id");
 
   try {
-    if (token !== import.meta.env.SECRET_API_TOKEN) {
-      return invalidRequestResponse("Invalid token", 401);
-    }
-
     if (!isRelativeUrl(redirectUrl)) {
       return invalidRequestResponse("URL must be relative!", 422);
     }
