3e311ce0e65ee9c5e158a3dd6aea198effa76f15
mariateresagraziano <marytex.dev@gmail.com>
2026-02-18 10:11:44 +0100

    feat: add migration

--- migrations/1771405797_fixLangLayout.ts
+++ migrations/1771405797_fixLangLayout.ts
@@ -1,37 +1,6 @@
 import { Client } from "@datocms/cli/lib/cma-client-node";
 
 export default async function (client: Client) {
-  console.log("Manage upload filters");
-
-  console.log('Update settings of plugin "Web Previews"');
-  await client.plugins.update("WSD86jZDQtqqM0v0QSUefA", {
-    parameters: {
-      frontends: [
-        {
-          name: "Production",
-          disabled: false,
-          customHeaders: [],
-          previewWebhook:
-            "https://template-site-astro.vercel.app/api/preview-links?token=4a8eaee12344fd81f6989fe9acb7f8bc4fc815210f0aec774b3fd7329efe35be",
-        },
-        {
-          name: "Local",
-          disabled: false,
-          customHeaders: [],
-          previewWebhook:
-            "http://localhost:4321/api/preview-links?token=4a8eaee12344fd81f6989fe9acb7f8bc4fc815210f0aec774b3fd7329efe35be",
-        },
-      ],
-      startOpen: true,
-      defaultViewports: [
-        { icon: "mobile-alt", name: "Mobile", width: 375, height: 667 },
-        { icon: "tablet-alt", name: "Tablet", width: 768, height: 1024 },
-        { icon: "desktop-alt", name: "Desktop", width: 1280, height: 800 },
-      ],
-      defaultSidebarWidth: 900,
-    },
-  });
-
   console.log("Update existing fields/fieldsets");
 
   console.log(
