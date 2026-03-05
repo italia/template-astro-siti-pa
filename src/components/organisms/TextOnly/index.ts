e7feb58deb30dd35614e6679c452b530e286ca02
mariateresagraziano <marytex.dev@gmail.com>
2026-03-05 14:56:45 +0100

    feat: add text only section

---
+++ src/components/organisms/TextOnly/index.ts
@@ -0,0 +1,1 @@
+export { default as TextOnly } from "./TextOnly.astro";
---
+++ src/components/organisms/TextOnly/types.ts
@@ -0,0 +1,5 @@
+import type { SectionBackground } from "@utils/background";
+
+export type TextOnlyProps = {
+  background?: SectionBackground;
+};
