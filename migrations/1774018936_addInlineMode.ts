0cf1f97d4821b268d20573de9d06c0ee14ec6884
mariateresagraziano <marytex.dev@gmail.com>
2026-03-20 16:12:13 +0100

    feat: add migration

--- migrations/1774018936_addInlineMode.ts
+++ migrations/1774018936_addInlineMode.ts
@@ -18,6 +18,26 @@ export default async function (client: Client) {
   console.log("Update existing fields/fieldsets");
 
   console.log(
+    'Update Multiple links field "Kpi element" (`kpi_element`) in block model "Highlight" (`highlight`)',
+  );
+  await client.fields.update("KrHrYC0RTSuiX8NvlBnD8A", {
+    validators: {
+      items_item_type: {
+        on_publish_with_unpublished_references_strategy: "fail",
+        on_reference_unpublish_strategy: "delete_references",
+        on_reference_delete_strategy: "delete_references",
+        item_types: ["MmI20ZtzQ5-AZGWwe6vUlw"],
+      },
+      size: { max: 4 },
+    },
+    appearance: {
+      addons: [],
+      editor: "links_select",
+      parameters: { filters: [] },
+    },
+  });
+
+  console.log(
     'Update Boolean field "Show inline card" (`show_inline_card`) in block model "Section card link list " (`card_link_list`)',
   );
   await client.fields.update("UPcGhKACTcy60zO_e98FrQ", { position: 1 });
